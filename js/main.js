bb648a7e79e2939795ffd526507cf3e11a247342
Adonis <adonisoliveira13@gmail.com>
2022-02-02 13:03:58 -0300

    tela login concluida - frontend

--- js/main.js
+++ js/main.js
@@ -12,3 +12,19 @@ var meuInterval = setInterval(function(){
 function setarRadio(indice){
     i = indice;
 }
+
+
+/* Login */
+
+document.getElementById('olho').addEventListener("mousedown", function() {
+    document.getElementById('senha').type = 'text';
+  });
+  
+  document.getElementById('olho').addEventListener('mouseup', function() {
+    document.getElementById('senha').type = 'password';
+  });
+  
+  // Para que o password não fique exposto apos mover a imagem.
+  document.getElementById('olho').addEventListener('mousemove', function() {
+    document.getElementById('senha').type = 'password';
+  });
